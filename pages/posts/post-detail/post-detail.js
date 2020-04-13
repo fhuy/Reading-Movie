ead5eb9f3d6bba20838ff6a7f2f59668ee90cb66
fhuy <2714342578@qq.com>
2020-04-13 15:08:29 +0800

    分享以及播放歌曲(0%)

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -40,20 +40,94 @@ Page({
   },
   onCollectionTap: function(){
     const postsCollected = wx.getStorageSync('posts_collected'),
-          postId = this.data.currentPostId;
-    console.log('postsCollected', postsCollected)
-    console.log('postId', postId)
-    this.setData({
-      collected: !this.data.collected
-    })
-    postsCollected[postId] = this.data.collected;
+          postId = this.data.currentPostId,
+          postCollected = !postsCollected[postId];
+    postsCollected[postId] = postCollected;
     wx.setStorageSync('posts_collected', postsCollected)
-    wx.showToast({
-      title: this.data.collected?"收藏成功":"取消成功"
+    this.setData({
+      collected: postCollected
     })
-    // console.log('collected', this.data.collected);
-    // console.log('', wx.getStorageSync('posts_collected'))
-    //应该是先把postsCollected里的属性值改变
-    //再
+    wx.showToast({
+      title: this.data.collected?"收藏成功":"取消成功",
+      duration: 1000
+    })
+    // this.showModal(postsCollected, postCollected);    
+  },
+  showModal: function(postsCollected, postCollected){
+    let that = this;
+    console.log('that', this);
+    wx.showModal({
+      title: '收藏',
+      content: postCollected?'收藏该文章':'取消收藏该文章',
+      showCancel: 'true',
+      cancelText: '取消',
+      cancelColor: '#333',
+      confirmText: '确认',
+      confirmColor: '#405f80',
+      success: function (res){
+        console.log('success-this', this)
+        if(res.confirm){
+          // 成功后，1.更新Storage；2.更新data的collected
+          wx.setStorageSync('posts_collected', postsCollected)
+          that.setData({
+            collected: postCollected
+          })
+        }
+      }
+    })
+  },
+  onShareTap: function(event){
+    console.log('event', event)
+    let that = this;
+    wx.showActionSheet({
+      itemList: [
+        "分享给微信好友",
+        "分享到朋友圈",
+        "分享到QQ",
+        "分享到微博",
+      ],
+      itemColor: "#405f80",
+      success: function(res){
+        console.log('that', that)
+        console.log('this',this);
+        console.log('res', res);
+        console.log('res.cancel',res.cancel);
+        console.log('tapIndex', res.tapIndex);
+        console.log('itemList[tapIndex]', itemList[res.tapIndex])
+        console.log('itemList[tapIndex]', that.itemList[res.tapIndex])
+      }
+    })
+  },
+  onShareAppMessage: function (res) {
+    return {
+      title: 'Reading&&Movies',
+      path: '/pages/posts/post-detail',
+      success: function (shareTickets) {
+        console.info(shareTickets + '成功');
+        // 转发成功
+      },
+      fail: function (res) {
+        console.log(res + '失败');
+        // 转发失败
+      },
+      complete:function(res){
+        // 不管成功失败都会执行
+      }
+    }
+  },
+  onMusicTap: function(){
+    console.log('hi')
+    // const BackgroundAudioManager = wx.getBackgroundAudioManager();
+    // BackgroundAudioManager.src = 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46';
+    // BackgroundAudioManager.title = '此时此刻'
+    // BackgroundAudioManager.epname = '此时此刻'
+    // BackgroundAudioManager.singer = '许巍'
+    // BackgroundAudioManager.coverImgUrl = 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000'
+
+    wx.playBackgroundAudio({
+      dataUrl: 'http://ws.stream.qqmusic.qq.com/C100002I8eGJ28BI17.m4a?fromtag=38',
+      title: '此时此刻',
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000'
+    })
   }
 })
--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -3,5 +3,22 @@ Page({
     wx.redirectTo({
       url: "../posts/post"
     });
-  }
+  },
+  onShareAppMessage: function (res) {
+    return {
+      title: 'Reading&&Movies',
+      path: '/pages/welcome/welcome',
+      success: function (shareTickets) {
+        console.info(shareTickets + '成功');
+        // 转发成功
+      },
+      fail: function (res) {
+        console.log(res + '失败');
+        // 转发失败
+      },
+      complete:function(res){
+        // 不管成功失败都会执行
+      }
+    }
+  },
 })
